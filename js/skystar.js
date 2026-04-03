aafdca53fe010fc36f9b5e0029d8e2c1d1c9a296
yezang <yezang@qq.com>
2026-04-04 00:29:28 +0800

    sfd

--- js/skystar.js
+++ js/skystar.js
@@ -18,6 +18,13 @@ var FLOAT_INTERVAL_MIN = 3800;
 var FLOAT_INTERVAL_MAX = 7200;
 var FLOAT_STAY_MS = 6500;
 var MAX_FLOATING = 5;
+var TITLE_STAGE_MS = 30000;
+var POEM_STAGE_MS = 30000;
+var POEM_LINES = [
+  '你抬头时，晚风正把星河吹亮',
+  '我闭上眼，心事却朝你生长',
+  '愿你所愿，都在晨光里安放'
+];
 
 function randomBetween(min, max) {
   return Math.random() * (max - min) + min;
@@ -85,25 +92,94 @@ function startFloatingLoop() {
   schedule();
 }
 
+var textoneWrap = document.querySelector('.textone');
+var texttwoWrap = document.querySelector('.texttwo');
+var textthreeWrap = document.querySelector('.textthree');
+
+var textone = textoneWrap.querySelector('h1');
+var texttwo = texttwoWrap.querySelector('h1');
+var textthree = textthreeWrap.querySelector('h1');
+
+function setBaseTextStyle() {
+  textone.style.color = '#E8F9FD';
+  textone.style.fontFamily = '楷体';
+  texttwo.style.color = '#E8F9FD';
+  texttwo.style.fontFamily = '楷体';
+  textthree.style.color = '#E8F9FD';
+  textthree.style.fontFamily = '楷体';
+}
+
+function showElement(el) {
+  el.classList.remove('stage-hidden');
+}
+
+function hideElement(el) {
+  el.classList.add('stage-hidden');
+}
+
+function resetDrop(el) {
+  el.classList.remove('stage-drop');
+}
+
+function dropOut(el) {
+  el.classList.add('stage-drop');
+}
+
+function startTimeline() {
+  setBaseTextStyle();
+
+  textone.innerHTML = '陈洁盈，整片星空将为你一人闪烁';
+  texttwo.innerHTML = '';
+  textthree.innerHTML = '';
+
+  showElement(textoneWrap);
+  hideElement(texttwoWrap);
+  hideElement(textthreeWrap);
+  resetDrop(textoneWrap);
+  resetDrop(texttwoWrap);
+  resetDrop(textthreeWrap);
+
+  window.setTimeout(function () {
+    dropOut(textoneWrap);
+
+    window.setTimeout(function () {
+      resetDrop(textoneWrap);
+      hideElement(textoneWrap);
+
+      textone.innerHTML = POEM_LINES[0];
+      texttwo.innerHTML = '';
+      textthree.innerHTML = '';
+      showElement(textoneWrap);
+
+      window.setTimeout(function () {
+        texttwo.innerHTML = POEM_LINES[1];
+        showElement(texttwoWrap);
+      }, 1800);
+
+      window.setTimeout(function () {
+        textthree.innerHTML = POEM_LINES[2];
+        showElement(textthreeWrap);
+      }, 3600);
+
+      window.setTimeout(function () {
+        dropOut(textoneWrap);
+        dropOut(texttwoWrap);
+        dropOut(textthreeWrap);
+
+        window.setTimeout(function () {
+          hideElement(textoneWrap);
+          hideElement(texttwoWrap);
+          hideElement(textthreeWrap);
+          textone.innerHTML = '';
+          texttwo.innerHTML = '';
+          textthree.innerHTML = '';
+          startFloatingLoop();
+        }, 900);
+      }, POEM_STAGE_MS);
+    }, 900);
+  }, TITLE_STAGE_MS);
+}
+
 window.addEventListener('load', function () {
-  startFloatingLoop();
+  startTimeline();
 });
-
-var textone = document.querySelector('.textone').querySelector('h1');
-var texttwo = document.querySelector('.texttwo').querySelector('h1');
-var textthree = document.querySelector('.textthree').querySelector('h1');
-
-textone.innerHTML = '陈洁盈，整片星空将为你一人闪烁';
-textone.style.color = '#E8F9FD';
-textone.style.fontFamily = '楷体';
-texttwo.style.color = '#E8F9FD';
-texttwo.style.fontFamily = '楷体';
-textthree.style.color = '#E8F9FD';
-textthree.style.fontFamily = '楷体';
-texttwo.innerHTML = '';
-
-window.setTimeout(function () {
-  textone.innerHTML = '祝护考顺利';
-  texttwo.innerHTML = '希望你考到自己理想的学校';
-  textthree.innerHTML = '加油！';
-}, 112500);
